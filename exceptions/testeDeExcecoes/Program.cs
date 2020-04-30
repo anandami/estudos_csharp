ee725e76f0dd8f3f0561cf1984ac5f1415c10055
anandami <ananda.anm@gmail.com>
2020-04-30 00:43:03 -0300

    Linux - criacao de repositorio novo, treino de excecoes

---
+++ exceptions/testeDeExcecoes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace testeDeExcecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando exceções e a pilha de chamadas");
+            Metodo1();
+        }
+        static void Metodo1(){
+            System.Console.WriteLine("Iniciando o método 1");
+            Metodo2();
+            System.Console.WriteLine("Finalizando o método 1");
+        }
+        static void Metodo2(){
+            System.Console.WriteLine("Iniciando o método 2");
+            Metodo3();
+            System.Console.WriteLine("Finalizando o método 2");
+        }
+
+        static void Metodo3(){
+            System.Console.WriteLine("Iniciando o método 3");
+            int dividendo = 10;
+            int divisor = 0;
+
+            try{
+               int resultado = dividendo / divisor;     
+            }
+            catch(DivideByZeroException){
+            
+            }
+            System.Console.WriteLine("Finalizando o último método");
+        }
+    }
+}
---
+++ exceptions/testeDeExcecoes2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace testeDeExcecoes2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste de Exceções e callstack");
+            Metodo();
+            CriacaoDeObjeto();
+        }
+        static void Metodo(){
+            try{
+              int result3 = TestaDivisao(10,0);      
+              System.Console.WriteLine(result3);
+              int result1 = TestaDivisao(10,10);
+              System.Console.WriteLine(result1);
+              int result2 = TestaDivisao(20,10);
+              System.Console.WriteLine(result2);
+              
+            }
+            catch(DivideByZeroException erro){
+                System.Console.WriteLine("Foi encontrato um erro no método. Finalizando.");
+                string message = erro.Message;
+                string track = erro.StackTrace;
+                System.Console.WriteLine(message);
+                System.Console.WriteLine(track);
+            }
+                        
+        }
+
+        static int TestaDivisao(int dividendo, int divisor){
+            
+            return dividendo/divisor;
+        }
+
+        static void CriacaoDeObjeto(){
+            try{
+                
+                NovaClasse novo = null;
+                novo.nome = "teste";
+                       
+            }
+            catch(NullReferenceException classeNula){
+                System.Console.WriteLine("Classe sem ref a objeto");
+                System.Console.WriteLine(classeNula.Message);
+                System.Console.WriteLine(classeNula.StackTrace);
+            }
+        }
+
+    }
+}
